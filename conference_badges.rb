4079d480f0ccfa90e627764bfab5832204145c83
Albert Chun <albert.s.chun@gmail.com>
2018-09-07 19:28:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(speakers)
-  room_assignments = 
+  room_assignments = speakers.each_with_index.collect { |speakers, index| "Hello, #{speakers}! You'll be assigned to room #{index+1}!"
 end
 
 def printer
